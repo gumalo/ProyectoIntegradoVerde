530eb920ef41315b93cf650e4de33264ff5a865a
gumalo <guillermo_m_96@hotmail.com>
2019-05-15 16:21:09 +0200

    Agregado clase Usuario y LoginInfo

---
+++ ProyectoEquipoVerde/ProyectoEquipoVerde/LoginInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoEquipoVerde
+{
+    static class LoginInfo
+    {
+        private static int idUserLogged;
+
+        public static int IdUserLogged { get => idUserLogged; set => idUserLogged = value; }
+    }
+}
---
+++ ProyectoEquipoVerde/ProyectoEquipoVerde/Usuario.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace ProyectoEquipoVerde
+{
+    class Usuario
+    {
+        private string id;
+        private string nombre;
+        private string nickname;
+        private string contrasenya;
+        private Image imagen;
+
+        public Usuario(string nombre, string nickname, string contrasenya, Image imagen)
+        {
+            this.nombre = nombre;
+            this.nickname = nickname;
+            this.contrasenya = contrasenya;
+            this.imagen = imagen;
+        }
+
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Nickname { get => nickname; set => nickname = value; }
+        public string Contrasenya { get => contrasenya; set => contrasenya = value; }
+        public Image Imagen { get => imagen; set => imagen = value; }
+
+        public static bool IniciadaSesion(int id)
+        {
+            
+        }
+    }
+}
