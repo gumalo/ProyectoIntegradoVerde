e4b8fc0ed167843294ddfd689471f5a85980a401
frloga01 <franlobogarcia@gmail.com>
2019-05-21 15:28:02 +0200

    clase crítica y película

--- ProyectoEquipoVerde/ProyectoEquipoVerde/Pelicula.cs
+++ ProyectoEquipoVerde/ProyectoEquipoVerde/Pelicula.cs
@@ -21,6 +21,7 @@ namespace ProyectoEquipoVerde
         private string descripcion;
         private string director;
         private Image cartel;
+        private double valoracion;
 
 
 
@@ -30,8 +31,8 @@ namespace ProyectoEquipoVerde
         public string Descripcion { get { return descripcion; } set { descripcion = value; } }
         public string Director { get { return director; } set { director = value; } }
         public Image Cartel { get { return cartel; } set { cartel = value; } }
-
         public DateTime Fecha { get => fecha; set => fecha = value; }
+        public double Valoracion { get { return valoracion; } set { valoracion = value; } }
 
         public Pelicula(int id_pelicula, string nombre, double calificacion, DateTime fecha, string descripcion, string director)
         {
@@ -47,14 +48,14 @@ namespace ProyectoEquipoVerde
 
 
 
-        public static List<Pelicula> VerInfoPeliPorNombre(string elNombre)
+        public static Pelicula VerInfoPeliPorNombre(string elNombre)
         {
-            List<Pelicula> lista = new List<Pelicula>();
-            string consulta = String.Format("SELECT * FROM Pelicula WHERE nombre_peli LIKE '{0}'", elNombre);
+            
+            string consulta = string.Format("SELECT * FROM Pelicula WHERE nombre_peli LIKE '{0}'", elNombre);
 
             MySqlCommand comando = new MySqlCommand(consulta, Conexion.Con);
             MySqlDataReader reader = comando.ExecuteReader();
-
+            Pelicula peli = new Pelicula();
             if (reader.HasRows)
             {
                 while (reader.Read())
@@ -62,25 +63,23 @@ namespace ProyectoEquipoVerde
                     byte[] img = (byte[])reader["portada"];
                     MemoryStream ms = new MemoryStream(img);
                     Image foto = Image.FromStream(ms);
-                    Pelicula peli = new Pelicula();
+                    
                     peli.id_pelicula = reader.GetInt16(0);
                     peli.nombre = reader.GetString(1);
-                    peli.
-
-
-
-
+                    peli.fecha = reader.GetDateTime(3); //la columna 2 es la de la imagen
+                    peli.descripcion = reader.GetString(4);
+                    peli.director = reader.GetString(5);                             
                     peli.cartel = Image.FromStream(ms);
-                    lista.Add(peli);
-
                 }
             }
             else
             {
                 MessageBox.Show("No se han encotrado películas con ese nombre");
             }
-            return lista;
+            return peli;
         }
+
+
 
         public static List<Pelicula> VerPelisPorFecha(DateTime fecha)
         {
@@ -93,42 +92,20 @@ namespace ProyectoEquipoVerde
 
             if (reader.HasRows)
             {
-
-                //De momento dejo esta mierda hasta ver cómo hacemos
                 while (reader.Read())
                 {
-                    Pelicula peli = new Pelicula(/*reader.GetInt16(0), reader.GetString(1), reader.GetString(2),
-                        reader.GetString(3), reader.GetString(4), reader.GetString(5), reader.GetDouble(6), reader.GetString(7),
-                        reader.GetDateTime(8), reader.GetBoolean(9), reader.GetString(10), reader.GetInt16(11)*/);
-                    lista.Add(peli);
-                }
-            }
-            else
-            {
-                MessageBox.Show("No se han encotrado películas con ese nombre");
-            }
-            return lista;
-        }
+                    Pelicula peli = new Pelicula();
+                    byte[] img = (byte[])reader["portada"];
+                    MemoryStream ms = new MemoryStream(img);
+                    Image foto = Image.FromStream(ms);
 
-        /*
-        public static List<Pelicula> VerPelisPorFecha(DateTime fecha)
-        {
-            string laFecha = fecha.ToString("yyyy-MM-dd");
-            List<Pelicula> lista = new List<Pelicula>();
-            string consulta = String.Format("SELECT * FROM Pelicula WHERE fecha = '{0}'", laFecha);
+                    peli.id_pelicula = reader.GetInt16(0);
+                    peli.nombre = reader.GetString(1);
+                    peli.fecha = reader.GetDateTime(3); //la columna 2 es la de la imagen
+                    peli.descripcion = reader.GetString(4);
+                    peli.director = reader.GetString(5);
+                    peli.cartel = Image.FromStream(ms);
 
-            MySqlCommand comando = new MySqlCommand(consulta, Conexion.Con);
-            MySqlDataReader reader = comando.ExecuteReader();
-
-            if (reader.HasRows)
-            {
-
-                //De momento dejo esto hasta ver cómo hacemos
-                while (reader.Read())
-                {
-                    //Pelicula peli = new Pelicula(reader.GetInt16(0), reader.GetString(1), reader.GetString(2),
-                        reader.GetString(3), reader.GetString(4), reader.GetString(5), reader.GetDouble(6), reader.GetString(7),
-                        reader.GetDateTime(8), reader.GetBoolean(9), reader.GetString(10), reader.GetInt16(11));
                     lista.Add(peli);
                 }
             }
@@ -140,49 +117,125 @@ namespace ProyectoEquipoVerde
         }
 
 
-
-
-        
         public List<Pelicula> VerTodas()
         {
-            return;
+            string laFecha = fecha.ToString("yyyy-MM-dd");
+            List<Pelicula> lista = new List<Pelicula>();
+            string consulta = "SELECT Pelicula.nombre_peli, Pelicula.anyo_peli, " +
+                "Pelicula.desc_peli, Pelicula.director_peli, " +
+                "AVG(Critica.valoracion_critica) " +
+                "AS media_valoracion, Pelicula.portada " +
+                "FROM Pelicula RIGHT JOIN Critica ON Critica.peli_critica=Pelicula.id_peli " +
+                "GROUP by Critica.peli_critica;";
+
+            MySqlCommand comando = new MySqlCommand(consulta, Conexion.Con);
+            MySqlDataReader reader = comando.ExecuteReader();
+
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    Pelicula peli = new Pelicula();
+                    byte[] img = (byte[])reader["portada"];
+                    MemoryStream ms = new MemoryStream(img);
+                    Image foto = Image.FromStream(ms);
+
+                    //peli.id_pelicula = reader.GetInt16(0);
+                    peli.nombre = reader.GetString(0);
+                    peli.fecha = reader.GetDateTime(1); //la columna 2 es la de la imagen
+                    peli.descripcion = reader.GetString(2);
+                    peli.director = reader.GetString(3);
+                    double lavaloracion = reader.GetDouble(4);
+                    peli.valoracion = Math.Round(lavaloracion,1);
+                    peli.cartel = Image.FromStream(ms);
+
+                    lista.Add(peli);
+                }
+            }
+            else
+            {
+                MessageBox.Show("No se han podido cargar películas");
+            }
+            return lista;
+            
         }
 
         public List<Pelicula> BuscarPeliculaPorFecha(DateTime fechaIni, DateTime fechaFin)
         {
-            return;
+            string laFecha = fecha.ToString("yyyy-MM-dd");
+            List<Pelicula> lista = new List<Pelicula>();
+            string consulta = String.Format("SELECT * FROM Pelicula WHERE fecha >= '{0}' AND fecha <='{1}'", fechaIni, fechaFin);
+
+            MySqlCommand comando = new MySqlCommand(consulta, Conexion.Con);
+            MySqlDataReader reader = comando.ExecuteReader();
+
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    Pelicula peli = new Pelicula();
+                    byte[] img = (byte[])reader["portada"];
+                    MemoryStream ms = new MemoryStream(img);
+                    Image foto = Image.FromStream(ms);
+
+                    peli.id_pelicula = reader.GetInt16(0);
+                    peli.nombre = reader.GetString(1);
+                    peli.fecha = reader.GetDateTime(3); //la columna 2 es la de la imagen
+                    peli.descripcion = reader.GetString(4);
+                    peli.director = reader.GetString(5);
+                    peli.cartel = Image.FromStream(ms);
+
+                    lista.Add(peli);
+                }
+            }
+            else
+            {
+                MessageBox.Show("No se han encotrado películas entre esas fechas");
+            }
+            return lista;
         }
 
         public List<Pelicula> BuscarPorDirector(string nomDirector)
         {
-            return;
+            string laFecha = fecha.ToString("yyyy-MM-dd");
+            List<Pelicula> lista = new List<Pelicula>();
+            string consulta = String.Format("SELECT * FROM Pelicula WHERE director_peli = '{0}'", nomDirector);
+
+            MySqlCommand comando = new MySqlCommand(consulta, Conexion.Con);
+            MySqlDataReader reader = comando.ExecuteReader();
+
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    Pelicula peli = new Pelicula();
+                    byte[] img = (byte[])reader["portada"];
+                    MemoryStream ms = new MemoryStream(img);
+                    Image foto = Image.FromStream(ms);
+
+                    peli.id_pelicula = reader.GetInt16(0);
+                    peli.nombre = reader.GetString(1);
+                    peli.fecha = reader.GetDateTime(3); //la columna 2 es la de la imagen
+                    peli.descripcion = reader.GetString(4);
+                    peli.director = reader.GetString(5);
+                    peli.cartel = Image.FromStream(ms);
+
+                    lista.Add(peli);
+                }
+            }
+            else
+            {
+                MessageBox.Show("No se han encotrado películas de" + nomDirector);
+            }
+            return lista;
         }
 
-        public List<Pelicula> BuscarPorNombre(string nomPeli)
-        {
-            return;
-        }
 
-        public int MostrarCalificacion(int idPeli)
-        {
-            return;
-        }
-
-        public string MostrarEtiqueta(int idPeli)
-        {
-            return;
-        }
-
-        public string MostrarSegundaEtiqueta(int idPeli)
-        {
-            return;
-        }
-
+        /* PENDIENTE
         public List<Pelicula> MostrarPeliculasPorAfinidad()
         {
             return;
         }
-
         */
 
 
@@ -190,7 +243,7 @@ namespace ProyectoEquipoVerde
         {
             double valoracionMedia = 0;
             double valoracion = 0;
-            string consulta = String.Format("SELECT AVG (valoracion_critica) FROM Critica WHERE peli_critica={0};", idPeli);
+            string consulta = String.Format("SELECT AVG (valoracion_critica) FROM Critica WHERE peli_critica={0} AND valoracion_critica IS NOT NULL;", idPeli);
             MySqlCommand comando = new MySqlCommand(consulta, Conexion.Con);
             MySqlDataReader reader = comando.ExecuteReader();
 
@@ -203,38 +256,69 @@ namespace ProyectoEquipoVerde
             }
             else
             {
-                MessageBox.Show("No se han encotrado valoraciones de esa palícula");
+                MessageBox.Show("No se han encotrado valoraciones de esa película");
             }
             valoracionMedia = Math.Round(valoracion,1);
             return valoracionMedia;
         }
 
-        public static List<Pelicula> VerPelisPorFecha(DateTime fecha)
-        {
-            string laFecha = fecha.ToString("yyyy-MM-dd");
-            List<Pelicula> lista = new List<Pelicula>();
-            string consulta = String.Format("SELECT * FROM Pelicula WHERE fecha = '{0}'", laFecha);
 
+        public static double ObtenerValoracionesMediasOrdenadas()
+        {
+
+            string consulta = "SELECT Pelicula.nombre_peli, " +
+                "ROUND(AVG(Critica.valoracion_critica),1) as " +
+                "media_valoracion FROM Critica left join Pelicula ON" +
+                " Critica.peli_critica = Pelicula.id_peli GROUP BY peli_critica;";
             MySqlCommand comando = new MySqlCommand(consulta, Conexion.Con);
             MySqlDataReader reader = comando.ExecuteReader();
 
             if (reader.HasRows)
             {
-
-                //De momento dejo esta mierda hasta ver cómo hacemos
                 while (reader.Read())
                 {
-                    Pelicula peli = new Pelicula(/*reader.GetInt16(0), reader.GetString(1), reader.GetString(2),
-                        reader.GetString(3), reader.GetString(4), reader.GetString(5), reader.GetDouble(6), reader.GetString(7),
-                        reader.GetDateTime(8), reader.GetBoolean(9), reader.GetString(10), reader.GetInt16(11)*/);
-                    lista.Add(peli);
+                   // Pendiente ver qué devuelve esta función
                 }
             }
             else
             {
-                MessageBox.Show("No se han encotrado películas con ese nombre");
+                MessageBox.Show("No se han encotrado valoraciones de esa película");
             }
-            return lista;
+            //double valoracionMedia = Math.Round(valoracion, 1);
+            /return valoracionMedia;
         }
+
+        public int ObtenerTagPelicula(int idPeli)
+        {
+            //Necesitamos determinar qué tag corresponde a cada número
+            string consulta = string.Format("SELECT tag_1 from Critica where peli_critica={0};", idPeli);
+            MySqlCommand comando = new MySqlCommand(consulta, Conexion.Con);
+            MySqlDataReader reader = comando.ExecuteReader();
+
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    int elTag = reader.GetInt32(0);
+                    switch (elTag)
+                    {
+                        case 1
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("No se han encotrado valoraciones de esa película");
+            }
+            valoracionMedia = Math.Round(valoracion, 1);
+
+        }
+
+
+
+
+
+
+
     }
 }
